16f80a9f1e9aa2844148a9f513fe0f5228e4263f
Jacobo Perez <jacoboperez@Jacobos-MacBook-Pro.local>
2018-05-07 12:08:42 -0600

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,53 @@ let friendsArray = [
             "5",
             "4",
             "1"]
-    }];
+    },
+    {
+        name: "Romeo",
+        photo: "http://i0.kym-cdn.com/photos/images/facebook/000/012/445/lime-cat.jpg",
+        scores: [
+            "2",
+            "1",
+            "5",
+            "3",
+            "3",
+            "4",
+            "1",
+            "4",
+            "4",
+            "1"]
+    },
+    {
+        name: "Chilaquil",
+        photo: "http://i0.kym-cdn.com/entries/icons/original/000/023/045/18581671_658423897682270_3810108845865843196_n.jpg",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"]
+    },
+    {
+        "name": "Chilaquiles",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Chilaquiles_Rojos.JPG/1200px-Chilaquiles_Rojos.JPG",
+        "scores": [
+        "2",
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3"
+        ]
+        }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,16 +6,49 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function(req, res){
+
+        // Create array to store the total differences between the current user
+        // and items in the friends array
+        arrayDifferences = [];
         
-        // get the current user scores
+        // get the current user's scores
         let currentUserScores = req.body.scores;
-        // Parse the scores to integers
+        // Parse each element in the array to an interger
         currentUserScores = currentUserScores.map(number => parseInt(number))
-        console.log(currentUserScores);
-        console.log(friendsData.length);
-        console.log("before", friendsData);
+        console.log("current", currentUserScores);
+        
+        // Loop through each friend in the api friends array
+        friendsData.forEach(friend => {
+            // Parse elements to integers
+            let friendScore = friend.scores;
+            friendScore = friendScore.map(number => parseInt(number))
+
+            // Substract the elements from the current user's scores and the friend's score
+            // return the absolute value
+            let totalDifference = currentUserScores.map((item, index) => {return Math.abs(item - friendScore[index])});
+
+            // Find the total difference by summing the elements in the array
+            totalDifference = totalDifference.reduce((acc, val) => {return acc + val});
+            console.log("Current total difference", totalDifference);
+
+            // Push this total difference to the arrayDifferences
+            arrayDifferences.push(totalDifference);
+        });
+
+        // Find the index of the element lowest value in the arrayDifferences,
+        // this corresponds to the index of the closest match in the api friends array
+        // We'll find the first intances only
+
+        const matchIndex = arrayDifferences.indexOf(Math.min.apply(null, arrayDifferences));
+        console.log("Match index", matchIndex);
+
+        // Send response with the closest match
+        console.log(friendsData[matchIndex]);
+        res.json(friendsData[matchIndex]);
+
+        // Push the current user's data after the comparison is made
         friendsData.push(req.body);
 
-        console.log("after", friendsData);
+        console.log("after push", friendsData);
     })
 };
